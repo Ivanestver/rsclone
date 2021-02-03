db730a1db9c2f33570ff4a665ee459e8d0bbc4c5
Ivan Patrenkov <bla@bla.bla>
2021-02-03 10:08:50 +0300

    fix: change armors prices

--- final_fantasy/main/src/classes/armories.js
+++ final_fantasy/main/src/classes/armories.js
@@ -8,13 +8,13 @@ export class Armory extends Object {
     }
 }
 
-export var Clothes = new Armory('Simple Clothes', 5, 20);
-export var LeatherArmory = new Armory('Leather Armor',  15, 50);
-export var BoneArmor = new Armory('Bone Armor',  15, 50);
-export var Corselet = new Armory('Corselet',  15, 50);
-export var Cuirass = new Armory('Cuirass',  15, 50);
-export var DarkArmor = new Armory('Dark Armor',  15, 50);
-export var ChaosArmor = new Armory('Chaos Armor',  15, 50);
-export var IronManArmor = new Armory('Iron Man Armor',  15, 50);
+export var Clothes = new Armory('Simple Clothes', 5, 80);
+export var LeatherArmory = new Armory('Leather Armor',  12, 165);
+export var BoneArmor = new Armory('Bone Armor',  15, 180);
+export var Corselet = new Armory('Corselet',  17, 220);
+export var Cuirass = new Armory('Cuirass',  20, 250);
+export var DarkArmor = new Armory('Dark Armor',  30, 700);
+export var ChaosArmor = new Armory('Chaos Armor',  40, 1300);
+export var IronManArmor = new Armory('Iron Man Armor',  50, 2500);
 
 export var armories = [Clothes, LeatherArmory, BoneArmor, Corselet, Cuirass, DarkArmor, ChaosArmor, IronManArmor];
